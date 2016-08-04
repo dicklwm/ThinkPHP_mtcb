2242659a97b6bc92db2f3a6a84a0a8c8d6d38b74
Min <dicklwm@163.com>
2016-08-05 00:19:08 +0800

    修改Index控制器
    Signed-off-by: Min <dicklwm@163.com>

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,4 +1,7 @@
 <?php
-return array(
-	//'配置项'=>'配置值'
+return array (
+		//开启显示页面Trace信息
+		'SHOW_PAGE_TRACE' => true ,
+		'TMPL_ACTION_ERROR'     =>   APP_PATH . 'Home/View/Public/dispatch_jump.html', // 默认错误跳转对应的模板文件
+		'TMPL_ACTION_SUCCESS'   =>  APP_PATH . 'Home/View/Public/dispatch_jump.html', // 默认成功跳转对应的模板文件
 );
