e747ca04188a4ad29b317b2c42944e07c834300a
Min <dicklwm@163.com>
2016-08-05 00:19:08 +0800

    添加Success和Error的弹出模态框模板
    Signed-off-by: Min <dicklwm@163.com>

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-    	dump( C('MODULE_ALLOW_LIST'    ));
+//     	$this->success("添加成功",'',5);
+// 			$this->error('dddssad','',5);
     }
 }
